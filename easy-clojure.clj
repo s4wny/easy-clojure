0bb24c9eba28137906e0690ddb29cda8049b64f6
Sony? <sony@4morefun.net>
2012-10-10 20:30:30 +0200

    Added to-int, PI, args and updated argument-helper

--- easy-clojure.clj
+++ easy-clojure.clj
@@ -1,15 +1,39 @@
 ;; Author Sony? aka Sawny (4morefun.net)
 
 
+;; Argument helper
 (defn arg
     "Returns argument i."
     {:author "Sony? aka Sawny"}
 
+    ([]
+        *command-line-args*)
+
     ([n]
         (nth *command-line-args* (dec n) nil))
+
     ([n errorMsg]
         (if (nil? (nth *command-line-args* (dec n) nil))
             (do
                 (println errorMsg)
                 (System/exit 0))
             (nth *command-line-args* (dec n)))))
+
+
+;; To-int; Conver string to int.
+(defn to-int [i]
+    (Integer. (re-find #"\d+" i)))
+
+
+
+;;;;;;;;;;;;;;;
+;;; Aliases ;;;
+;;;;;;;;;;;;;;;
+
+
+;; PI
+(def PI
+    (Math/PI))
+
+;;Args
+(def args *command-line-args*)
